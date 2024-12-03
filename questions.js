65a8dab582594d20f201e34890e9be2ad9a3f704
Jonathan Lee <kutsu89@gmail.com>
2024-12-03 12:18:23 +0000

    initial commit and added 1st question

--- questions.js
+++ questions.js
@@ -1,7 +1,14 @@
 // questions.js
 
 const questions = [
-  // Add your questions here
+  {
+    question_text:
+      "Which of the following is NOT a primitive data type in JavaScript?",
+    question_type: "multiple_choice",
+    options: ["Boolean", "String", "Array", "Number"],
+    correct_option: 2,
+    correct_explanation: "Array is not a primitive data type; it is an object.",
+  },
 ];
 
 // Don't worry about this, we're just exporting the questions
